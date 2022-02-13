8e4ec3d3dac69536bbd3808a7f80060e7623c43c
atgarcia78 <a.tgarc@gmail.com>
2022-02-13 20:23:06 +0100

    upt

--- utils.py
+++ utils.py
@@ -279,7 +279,7 @@ def init_logging(file_path=None):
 def init_argparser():
     
  
-    UA_LIST = ["Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:98.0) Gecko/20100101 Firefox/98.0"]
+    UA_LIST = ["Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:99.0) Gecko/20100101 Firefox/99.0"]
 
 
     parser = argparse.ArgumentParser(description="Async downloader videos / playlist videos HLS / HTTP")
