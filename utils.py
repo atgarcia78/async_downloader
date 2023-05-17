23ba8bafb276c5279981d9f61a0e3f790b29cd8b
atgarcia78 <a.tgarc@gmail.com>
2023-05-17 19:46:47 +0200

    upt

--- utils.py
+++ utils.py
@@ -71,6 +71,27 @@ try:
 except Exception:
     yt_dlp = None
 
+
+# ***********************************+
+# ************************************
+
+from importlib.machinery import SOURCE_SUFFIXES, FileFinder, SourceFileLoader
+from importlib.util import module_from_spec
+
+
+_loader_details = [(SourceFileLoader, SOURCE_SUFFIXES)]
+
+
+def load_module(name, path: str):
+    finder = FileFinder(path, *_loader_details)
+    spec = finder.find_spec(name)
+    if not spec or not spec.loader:
+        raise ImportError(f"no module named {name}")
+    mod = module_from_spec(spec)
+    spec.loader.exec_module(mod)
+    return mod
+
+
 assert cast
 
 PATH_LOGS = Path(Path.home(), "Projects/common/logs")
