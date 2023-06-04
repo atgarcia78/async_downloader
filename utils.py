d6470a94d1e279aa9b254f40c2d9e6d8cc1afca8
atgarcia78 <a.tgarc@gmail.com>
2023-06-04 04:07:35 +0200

    2023-06-04
    Signed-off-by: atgarcia78 <a.tgarc@gmail.com>

--- utils.py
+++ utils.py
@@ -811,7 +811,7 @@ def init_argparser():
         action=ActionNoYes,
         default="6800"
     )
-    parser.add_argument("--subt", action=ActionNoYes, default=False)
+    parser.add_argument("--subt", action=ActionNoYes, default=True)
     parser.add_argument("--nosymlinks", action="store_true", default=False)
     parser.add_argument("--http-downloader", choices=["native", "aria2c", "saldl"],
                         default="aria2c")
