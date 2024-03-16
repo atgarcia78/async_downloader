c1e1ac0bd8809c6d32eded9a101e84ad3f8e5367
atgarcia78 <a.tgarc@gmail.com>
2024-03-16 12:24:08 +0100

    2024-03-16
    Signed-off-by: atgarcia78 <a.tgarc@gmail.com>

--- utils.py
+++ utils.py
@@ -133,7 +133,7 @@ CONF_ARIA2C_N_CHUNKS_CHECK_SPEED = _min // 4  # 60
 CONF_ARIA2C_TIMEOUT_INIT = 20
 CONF_INTERVAL_GUI = 0.2
 
-CONF_ARIA2C_EXTR_GROUP = ["doodstream", "odnoklassniki"]
+CONF_ARIA2C_EXTR_GROUP = ["doodstream", "odnoklassniki", "heretv"]
 CONF_AUTO_PASRES = ["doodstream"]
 
 CLIENT_CONFIG = {
